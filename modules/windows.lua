b71ec3dd7f4b2d495f7bfb74aaf6d454695639ef
lace-wing <lycrlsu01@gmail.com>
2024-05-22 20:48:18 +0800

    windows shells

--- modules/windows.lua
+++ modules/windows.lua
@@ -1,1 +1,16 @@
-return {}
+return {
+  shell = {
+    default = {
+      name = "nushell",
+      path = "C:\\Users\\steve\\AppData\\Local\\Programs\\nu\\bin\\nu.exe",
+    },
+    cmd = {
+      name = "CMD",
+      path = "C:\\WINDOWS\\system32\\cmd.exe",
+    },
+    pwsh = {
+      name = "PowerShell",
+      path = "C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
+    }
+  },
+}
